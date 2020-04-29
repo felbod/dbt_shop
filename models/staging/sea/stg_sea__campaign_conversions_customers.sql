c7762ba1d9e633274fa115781947d8eea4976561
Felix Bodeewes <fbo@forium.de>
2020-04-29 17:40:22 +0200

    Updated: Formatting

--- models/staging/sea/stg_sea__campaign_conversions_customers.sql
+++ models/staging/sea/stg_sea__campaign_conversions_customers.sql
@@ -1,43 +1,37 @@
 
 with campaign_conversions as (
-
   select *, 'Google Ads' as platform
-
     from {{ref('stg_google_ads__campaign_conversions')}}
-
-union all
-
+  union all
   select * except (rank), 'Microsoft Ads' as platform
-
-    from {{ref('stg_microsoft_ads__campaign_conversions')}}
-)
+    from {{ref('stg_microsoft_ads__campaign_conversions')}})
 
 select
-  date_day,
-  platform,
-  account_id,
-  campaign_id,
-  sum (signups) as signups,
-  sum (conversion_value_eur) as conversion_value_eur,
-  sum (is_new_customers * conversion_value_eur)
-    as conversion_value_eur_new_customers,
-  sum (if (is_new_customers = 1, 0, conversion_value_eur))
-    as conversion_value_eur_old_customers,
-  sum (if (is_new_customers = 1, 0, conversions))
-    as clicks_old_customers, -- Assumption: 1 click per conversion by old customers
-  sum (starts) as starts,
-  sum (is_new_customers * starts) as starts__new_customers,
-  sum (if (is_new_customers = 1, 0, starts))
-    as starts__old_customers,
-  sum (sales) as sales,
-  sum (is_new_customers * sales) as sales__new_customers,
-  sum (if (is_new_customers = 1, 0, sales))
+  date_day
+  , platform
+  , account_id
+  , campaign_id
+  , sum (signups) as signups
+  , sum (conversion_value_eur) as conversion_value_eur
+  , sum (is_new_customers * conversion_value_eur)
+    as conversion_value_eur_new_customers
+  , sum (if (is_new_customers = 1, 0, conversion_value_eur))
+    as conversion_value_eur_old_customers
+  , sum (if (is_new_customers = 1, 0, conversions))
+    as clicks_old_customers -- Assumption: 1 click per conversion by old customers
+  , sum (starts) as starts
+  , sum (is_new_customers * starts) as starts__new_customers
+  , sum (if (is_new_customers = 1, 0, starts))
+    as starts__old_customers
+  , sum (sales) as sales
+  , sum (is_new_customers * sales) as sales__new_customers
+  , sum (if (is_new_customers = 1, 0, sales))
     as sales__old_customers
 
 from campaign_conversions
 
 group by
-  date_day,
-  platform,
-  account_id,
-  campaign_id
+  date_day
+  , platform
+  , account_id
+  , campaign_id
