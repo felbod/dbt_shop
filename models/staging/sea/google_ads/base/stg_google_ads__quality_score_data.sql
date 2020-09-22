0aa9918437d1265c387ff81bf902e5ef14de46c6
Felix Bodeewes <fbo@forium.de>
2020-09-22 08:24:19 +0200

    Added: QS strings

--- models/staging/sea/google_ads/base/stg_google_ads__quality_score_data.sql
+++ models/staging/sea/google_ads/base/stg_google_ads__quality_score_data.sql
@@ -1,25 +1,6 @@
-
-/* https://docs.getdbt.com/docs/building-a-dbt-project/building-models/configuring-incremental-models/
-
-  {{
-    config(
-        materialized='incremental'
-    )
-  }}
-
-  select
-    *,
-    my_slow_function(my_column)
-
-  from raw_app_data.events
-
-  {% if is_incremental() %}
-
-  -- this filter will only be applied on an incremental run
-  where event_time > (select max(event_time) from {{ this }})
-
-  {% endif %}
-
+/*
+  example for incremental models
+  https://docs.getdbt.com/docs/building-a-dbt-project/building-models/configuring-incremental-models/
 */
 
 {{
@@ -35,6 +16,8 @@ from
   `planar-depth-242012.google_ads__transfer.Criteria_8644635112`
 
 where
+
+  -- only save criteria with quality score, i.e. keywords
   HasQualityScore is true
 
   {% if is_incremental() %}
@@ -43,5 +26,3 @@ where
   and _DATA_DATE > (select max(_DATA_DATE) from {{ this }})
 
   {% endif %}
-
--- limit 1
--- models/staging/sea/google_ads/base/stg_google_ads__quality_score_strings.sql
+++ models/staging/sea/google_ads/base/stg_google_ads__quality_score_strings.sql
@@ -4,36 +4,39 @@ select
   , account_name
   , campaign_name
   , adgroup_name
---  , display_name
+  , display_name
+  , replace(lower(strings), "+" , "") as string
 
 /*
   , account_id
   , campaign_id
   , adgroup_id
-  , keyword_id
+--  , keyword_id
 */
 
-  , date_year
+--  , date_year
 --  , date_isoyear
---  , date_quarter
-  , date_month
---  , date_isoweek
+  , date_quarter
+--  , date_month
 --  , date_week
+--  , date_isoweek
 --  , date_day
 
   , sum(impressions) as impressions
   , sum(quality_score * impressions) as quality_score_impressions
-  , sum(quality_score) as quality_score
+--  , sum(quality_score) as quality_score
 
+/*
   , sum(quality_score_calc) as quality_score_calc
   , sum(creative_quality_score_calc) as creative_quality_score_calc
   , sum(landingpage_quality_score_calc) as landingpage_quality_score_calc
   , sum(ctr_quality_score_calc) as ctr_quality_score_calc
+*/
 
   , sum(quality_score_calc_impressions) as quality_score_calc_impressions
-  , sum(creative_quality_score_calc_impressions) as creative_quality_score_calc_impressions
-  , sum(landingpage_quality_score_calc_impressions) as landingpage_quality_score_calc_impressions
-  , sum(ctr_quality_score_calc_impressions) as ctr_quality_score_calc_impressions
+--  , sum(creative_quality_score_calc_impressions) as creative_quality_score_calc_impressions
+--  , sum(landingpage_quality_score_calc_impressions) as landingpage_quality_score_calc_impressions
+--  , sum(ctr_quality_score_calc_impressions) as ctr_quality_score_calc_impressions
 
   , sum(creative_quality_score_rel_impressions) as creative_quality_score_rel_impressions
   , sum(landingpage_quality_score_rel_impressions) as landingpage_quality_score_rel_impressions
@@ -43,14 +46,22 @@ select
   , sum(landingpage_quality_score_opt_impressions) as landingpage_quality_score_opt_impressions
   , sum(ctr_quality_score_opt_impressions) as ctr_quality_score_opt_impressions
 
-from {{ref('stg_google_ads__quality_score_base')}}
+from
+  (
+    select
+      split(display_name, " ") as keywords
+      , *
+    from {{ref('stg_google_ads__quality_score_base')}}
+    )
+  , unnest(keywords) as strings
 
 group by
     brand_name
   , account_name
   , campaign_name
   , adgroup_name
---  , display_name
+  , display_name
+  , string
 
 /*
   , account_id
@@ -59,16 +70,17 @@ group by
   , keyword_id
 */
 
-  , date_year
---  , date_quarter
+--  , date_year
+  , date_quarter
 --  , date_isoyear
-  , date_month
+--  , date_month
 --  , date_week
 --  , date_isoweek
 --  , date_day
 
 order by
     brand_name
-  , date_month desc
+    , date_quarter desc
+--  , date_month desc
 --  , date_week desc
 --  , date_day desc
